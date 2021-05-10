fc2780670ff8838a65244f18d95566c05cb02169
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-10 11:51:17 +0530

    Scopes, let keyword

---
+++ variables.js
@@ -0,0 +1,15 @@
+// let keyword
+let total = 0;
+for(let i=0; i<10; i++){
+    total += i;
+}
+console.log("Total:-",total); // 45
+try{
+    console.log(i); // Error
+} catch (e){
+
+}
+
+// const Keyword
+const sum = "Sum variable";
+console.log("Sum:-",sum);
