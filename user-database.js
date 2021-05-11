3dc8a08ecff762fb83566f00853d7c5ad3f97376
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-11 12:04:17 +0530

    user-database.js completed

--- user-database.js
+++ user-database.js
@@ -14,4 +14,116 @@
 // 2) remove user
 // 3) update user data
 // 4) getUser
-// 5) getUserFirstCondingLang -  fetch first coding language of a user.
+// 5) getUserNCondingLang 
+        //-  fetch first coding language of a user.
+
+const input = require('prompt-sync')();
+const print = (message) => console.log(message);
+
+const arrInput = (type) => {
+    const totalInputs = input(`Number of ${type} you know:- `);
+    const result = []
+    for( let i = 0; i < totalInputs; i++ ){
+        result.push( input(`Enter ${i+1}th Input:- `) );
+    }
+    return result;
+}
+
+const birthdayInput = () => {
+    print("Enter your Birthdate:-");
+    const date = input("Date:- ");
+    const month = input("Month:- ");
+    const year = input("Year:- ");
+    return `${date}/${month}/${year}`;
+}
+
+const userData = {
+
+    length: 0,
+    addUser: () => {
+        const name = input("Enter your full name:- ");
+        const city = input("Enter your city:- ");
+        const birthDate = birthdayInput();
+        const skills = {
+            codingLang: arrInput("Coding Languages"),
+            softSkills: arrInput("Soft Skills"),
+            languages: arrInput("Languages"),
+        };
+        const user = {name, city, birthDate, skills};
+        userData[userData.length] = user;
+        userData.length++;
+    },
+    deleteUser: () => {
+        const index = input("Enter user index to delete:- ")
+        print("Deleting user:- ", userData[index]);
+        delete userData.index;
+        userData.length--;
+    },
+    updateUserCity: () => {
+        const index = input("Enter user index to update:- ");
+        const newCity = input("Enter new city:- ");
+        userData[index].city = newCity;
+        print("Updated");
+    },
+    getUser: () => {
+        const index = input("Enter user index:- ");
+        print(userData[index]);
+    },
+    getUserCondingLang: () => {
+        const index = input("Enter user index:- ");
+        const {skills: {codingLang}} = userData[index];
+        print(codingLang[0]);
+    },
+};
+
+const main = () => {
+    let running = true;
+    while(running){
+        print("Welcome to user database");
+        print("Select an option:-");
+        print("1) Add User");
+        print("2) Remove User");
+        print("3) Update User City");
+        print("4) Get User");
+        print("5) Get first coding language");
+        print("6) Total users");
+        print("7) Print database");
+        print("8) Exit");
+        const option = input("Enter your option:- ");
+        switch(option){
+            case '1':
+                userData.addUser();
+                input("Press Enter to move ahead.")
+                break;
+            case '2':
+                userData.deleteUser();
+                input("Press Enter to move ahead.")
+                break;
+            case '3':
+                userData.updateUserCity();
+                input("Press Enter to move ahead.")
+                break;
+            case '4':
+                userData.getUser();
+                input("Press Enter to move ahead.")
+                break;
+            case '5':
+                userData.getUserCondingLang();
+                input("Press Enter to move ahead.")
+                break;
+            case '6':
+                print(userData.length);
+                input("Press Enter to move ahead.")
+                break;
+            case '7':
+                print(userData);
+                input("Press Enter to move ahead.")
+                break;
+            case '8':
+                running = false;
+                break;
+        }
+    }
+}
+
+main();
