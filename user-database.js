b54704a89c2f777b720c78af10043f258dc0b1ab
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-11 10:55:47 +0530

    user-input, user-database-task

---
+++ user-database.js
@@ -0,0 +1,17 @@
+// User Database
+// Store user Data in object
+// Data Points to store
+// 1) full name
+// 2) City
+// 3) Birth date
+// 4) Skills:
+//      a) codingLang - Coding Languages array
+//      b) softSkills - Soft skills array
+//      c) languages - array
+
+// Functions
+// 1) add user
+// 2) remove user
+// 3) update user data
+// 4) getUser
+// 5) getUserFirstCondingLang -  fetch first coding language of a user.
