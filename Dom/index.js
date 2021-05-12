c9cf8d0b8408f31471c24be709619c92bd3555a5
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-12 11:03:26 +0530

    class + dom manipulations

---
+++ Dom/index.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const todoList = document.getElementById("todoList");
+
+let task;
+
+const handleInput = ({target}) => {
+    task = target.value ;
+};
+
+const handleClick = () => {
+    const li = document.createElement('li');
+    li.innerText = task;
+    todoList.appendChild(li);
+}
+
+input.addEventListener("change", handleInput);
+btn.addEventListener("click", handleClick);
