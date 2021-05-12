89e78659b28ff914cad691e14b764b3349f6d74c
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-12 11:52:43 +0530

    Completed the todo-list

--- Dom/index.js
+++ Dom/index.js
@@ -4,16 +4,21 @@ const todoList = document.getElementById("todoList");
 
 let task;
 
+const removeElement = (event) => {
+    event.srcElement.parentElement.remove();
+}
+
 const handleInput = ({target}) => {
     task = target.value ;
 };
 
 const handleClick = () => {
     const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.addEventListener("click", removeElement);
     li.innerText = task;
+    li.appendChild(btn);
     todoList.appendChild(li);
-    // todoList.remove();
-    // todoList.removeChild( todoList.childNodes[] );
 }
 
 input.addEventListener("change", handleInput);
