2bf7e588abd7714387e963650a282725b726477e
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-05-13 12:29:53 +0530

    object-constructor array.splice array.slice

--- array-functions.js
+++ array-functions.js
@@ -1,18 +1,24 @@
 const arr = [1,2,3,4,5,6,7,9,4,1,56,7,651,68,4,51,68];
 
-/*
+// console.log( arr.slice(6,10) ); // [7,9,4,1]
+
+const removedSlice = arr.splice(6,4, "New Elements");
+console.log(arr, removedSlice);
+
+console.log(arr.indexOf("New Elements"));
+console.log(arr.indexOf(651));
+console.log(arr.indexOf(999));
+
 for(let i = 0; i<arr.length; i++){
     if(arr[i]%2 === 0) console.log('even');
     else console.log('odd');
 }
-*/
 
 const callback = (element) => {
     if(element%2 === 0) return ['Callback','Even'];
     return ['Callback','Odd'];
 }
 
-/*
 const map = (input, callback) => {
     let output = [];
     for(let i = 0; i<input.length; i++){
@@ -20,27 +26,19 @@ const map = (input, callback) => {
     }
     return output;
 }
-*/
 
-/*
 const mapped = arr.map(callback);   //forEach
 
 console.log(mapped);
-*/
 
-/*
 console.log(map(arr, callback));
-*/
 
-/*
 const filter = arr.filter(
     (element) => element%2!==0
 );
 
 console.log(filter);
-*/
 
-/*
 const parseObject = (user) => {
     const props = Object.keys(user);
     console.log(user.hasOwnProperty(props[0]));
@@ -50,8 +48,4 @@ const parseObject = (user) => {
 
 parseObject({name: "mihir", age: 22});
 
-*/
-
-/*
 console.log(Object.keys(arr));
-*/
